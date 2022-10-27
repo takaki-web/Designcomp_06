a10bfcb0f7483784f0e7a7330782903902998e4d
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-10-27 10:44:15 +0900

    js-slick作成

--- js/script.js
+++ js/script.js
@@ -7,3 +7,42 @@ $('a[href^="#"]').click(function () {
   $("body,html").animate({ scrollTop: position }, speed, "swing");
   return false;
 });
+
+$(function () {
+  $("#js-p-index-fv__slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    slidesToShow: 1,
+    speed: 4000,
+  });
+});
+
+$(function(){
+  function sliderSetting(){
+
+      var width = $(window).width();
+
+      if(width <= 500){
+          $('#js-p-index-various__slider').not('.slick-initialized').slick({
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            slidesToShow: 1,
+            speed: 4000,
+            centerMode: true,
+            centerPadding: "52px",
+            dots: true,
+                  });
+      } else {
+          $('#js-p-index-various__slider.slick-initialized').slick('unslick');
+      }
+  }
+
+  sliderSetting();
+
+  $(window).resize( function() {
+      sliderSetting();
+  });
+});
